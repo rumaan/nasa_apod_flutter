4092a0689ccc94c3e3c58c4efc64c5432d722b7e
payam <pzahedi1995@yahoo.com>
2019-10-12 20:34:43 +0330

    ApodException created

---
+++ lib/api/apod_exception.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/foundation.dart';
+
+/// [ApodException] created for throw apod exception it can be base class and
+/// in the future other Exception classes can extend from it.
+class ApodException implements Exception {
+  final String message;
+  final StackTrace stackTrace;
+
+  ApodException({
+    @required this.message,
+    this.stackTrace,
+  });
+
+  @override
+  String toString() {
+    return 'Some thing is Wrong: \n $message';
+  }
+
+}
