03113eb2d892cd958e9fb96da1a63fa0ebb136b1
rumaan <rumaankalander@gmail.com>
2019-10-14 01:32:34 +0530

    removed unused imports

--- lib/utils/permission_utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:permission_handler/permission_handler.dart';
-
-permissionCheck() async {
-  if (await PermissionHandler()
-          .checkPermissionStatus(PermissionGroup.storage) !=
-      PermissionStatus.granted) {
-    await PermissionHandler().requestPermissions([PermissionGroup.storage]);
-  }
-}
